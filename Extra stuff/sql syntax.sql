1e5f709075911e0770d1fd9a4f11b8d2a4554612
Hossén Rahimzadegan <57917112+acerd44@users.noreply.github.com>
2023-12-29 21:23:22 +0100

    Add files via upload

---
+++ Extra stuff/sql syntax.sql
@@ -0,0 +1,60 @@
+SELECT 
+  * 
+FROM 
+  Guest;
+
+SELECT 
+  COUNT(*) AS AmountOfGuests 
+FROM 
+  Guest;
+
+SELECT 
+  g.Name AS [Guest Name], 
+  i.TotalSum AS [Sum], 
+  i.IsPaid AS [Invoice Paid off] 
+FROM 
+  Guest AS g 
+  INNER JOIN Invoice AS i ON i.GuestId = g.Id;
+
+SELECT 
+  g.Name AS [Guest Name], 
+  i.TotalSum AS [Sum], 
+  i.PaidDate AS [Paid Date],
+  i.IsPaid AS [Invoice Paid off] 
+FROM 
+  Guest AS g 
+  INNER JOIN Invoice AS i ON i.GuestId = g.Id
+WHERE
+  i.TotalSum > 100;
+
+SELECT 
+  * 
+FROM 
+  Guest g 
+  INNER JOIN Booking b ON b.GuestId = g.Id 
+  INNER JOIN Invoice i on i.GuestId = g.Id;
+
+SELECT 
+  * 
+FROM 
+  Guest g 
+  INNER JOIN Booking b ON b.GuestId = g.Id 
+ORDER BY 
+  g.Id;
+
+SELECT 
+  Address, 
+  COUNT(Address) AS GuestsWithSameAddress 
+FROM 
+  Guest 
+GROUP BY 
+  [Address] 
+ORDER BY 
+  [GuestsWithSameAddress] DESC;
+
+SELECT
+  *
+FROM
+  Room
+WHERE
+  Beds = 2;
